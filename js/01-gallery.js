dedcd0db1f2898ca6813bf963d747eff1cf684aa
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-01-26 11:28:12 +0100

    260124

---
+++ js/01-gallery.js
@@ -0,0 +1,36 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const gallery = document.querySelector('.gallery'); 
+
+for (let item of galleryItems) {
+    const galleryItem = document.createElement('div');
+    galleryItem.classList.add('gallery__item');
+
+    const galleryLink = document.createElement('a');
+galleryLink.classList.add('gallery__item');
+galleryLink.href = item.original;
+
+const galleryImage = document.createElement('img');
+galleryImage.classList.add('.gallery__image');
+galleryImage.src = item.preview;
+galleryImage.alt = item.description;
+galleryImage.dataset.source = item.original;
+
+galleryLink.appendChild(galleryImage);
+galleryItem.appendChild(galleryLink);
+gallery.appendChild(galleryItem);
+}
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+  
+    const lightbox = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+    `);
+  
+    lightbox.show();
+  });
+
+
+console.log(galleryItems);
+
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://vitatro.github.io/goit-js-hw-07/
