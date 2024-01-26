13cb96bcb1ebfab035c07dcfdfcf5822a5421655
VitaTro <144832571+VitaTro@users.noreply.github.com>
2024-01-26 17:55:01 +0100

    2601245

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery'); 
+
+for (let item of galleryItems) {
+    const itemGallery = document.createElement('div');
+    itemGallery.classList.add('gallery__item');
+
+    const linkGallery = document.createElement('a');
+    linkGallery.classList.add('gallery__link');
+    linkGallery.href = item.original;
+
+const imageGallery = document.createElement('img');
+imageGallery.classList.add('gallery__image');
+imageGallery.src = item.preview;
+imageGallery.alt = item.description;
+imageGallery.dataset.source = item.original;
+
+linkGallery.append(imageGallery);
+itemGallery.append(linkGallery);
+gallery.append(itemGallery);
+};
 
 
 
